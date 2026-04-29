bd7813ce976931968753bf5e0dc27cda4691d325
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-04-29 19:11:13 -0400

    feat: remove logger from getQueueAdapter and add unit tests for QueueFactory

--- src/queues/queue.factory.ts
+++ src/queues/queue.factory.ts
@@ -8,7 +8,6 @@ import { LocalWorkerAdapter } from "./adapters/LocalWorkerAdapter.js";
 import { ConfigRegistry } from "../config/ConfigRegistry.js";
 import type { QueueOptions } from "../core/interfaces/queue.interface.js";
 import { DefaultInProcessAdapter } from "./adapters/DefaultInProcessAdapter.js";
-import { getLogger } from "../logger/logger.factory.js";
 
 /**
  * @description Factory para obtener el adaptador de colas activo.
@@ -16,20 +15,12 @@ import { getLogger } from "../logger/logger.factory.js";
  * Si no hay ninguno, evalúa las opciones para decidir el mejor default.
  */
 export function getQueueAdapter(): QueueAdapter {
-  const logger = getLogger();
-
   if (!container.has(QUEUE_ADAPTER_TOKEN)) {
     const config = ConfigRegistry.get<QueueOptions>("queue_user_config") || {};
 
     if (config.strategy === "worker-pool") {
-      logger.debug(
-        "[FastifyKit Queues] Utilizando LocalWorkerAdapter (Multihilo) por defecto.",
-      );
       container.registerClass(QUEUE_ADAPTER_TOKEN, LocalWorkerAdapter);
     } else {
-      logger.debug(
-        "[FastifyKit Queues] Utilizando DefaultInProcessAdapter por defecto (NO RECOMENDADO PARA CALCULOS INTENSIVOS, SOLO PARA I/O Y MVP).",
-      );
       container.registerClass(QUEUE_ADAPTER_TOKEN, DefaultInProcessAdapter);
     }
   }
