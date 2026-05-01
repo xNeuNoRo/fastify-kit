df28abeb90e8e57688d2611cc1a7de62203db8de
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-04-30 23:14:41 -0400

    feat: add CommandHandler and QueryHandler decorators for CQRS implementation

---
+++ src/cqrs/decorators/handler.decorators.ts
@@ -0,0 +1,48 @@
+import { container } from "../../container/DIContainer.js";
+import { FastifyKitMetadata } from "../../http/decorators/types.js";
+import { Constructor } from "../../http/routing/scanner/index.js";
+import { getCqrsHandlerToken } from "../utils/cqrs-token.util.js";
+
+/**
+ * @description Decorador para marcar una clase como el handler de un Comando específico.
+ * @param commandClass La clase del Comando que este handler procesará.
+ */
+export function CommandHandler(commandClass: Constructor) {
+  return function <T extends new (...args: any[]) => any>(
+    target: T,
+    context: ClassDecoratorContext,
+  ) {
+    if (context.kind !== "class") {
+      throw new Error(
+        `[FastifyKit CQRS] @CommandHandler solo puede usarse en clases.`,
+      );
+    }
+
+    (context.metadata as FastifyKitMetadata).cqrsHandler = true;
+
+    const token = getCqrsHandlerToken(commandClass);
+    container.registerClass(token, target);
+  };
+}
+
+/**
+ * @description Decorador para marcar una clase como el handler de una Query específica.
+ * @param queryClass La clase de la Query que este handler procesará.
+ */
+export function QueryHandler(queryClass: Constructor) {
+  return function <T extends new (...args: any[]) => any>(
+    target: T,
+    context: ClassDecoratorContext,
+  ) {
+    if (context.kind !== "class") {
+      throw new Error(
+        `[FastifyKit CQRS] @QueryHandler solo puede usarse en clases.`,
+      );
+    }
+
+    (context.metadata as FastifyKitMetadata).cqrsHandler = true;
+
+    const token = getCqrsHandlerToken(queryClass);
+    container.registerClass(token, target);
+  };
+}
